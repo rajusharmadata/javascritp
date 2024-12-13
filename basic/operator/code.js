ec28faf0e66002cf442b5c5b813a625284dec784
rajusharmadata <rajusharmadata123@gmail.com>
2024-12-13 20:53:39 +0530

    add

--- basic/operator/code.js
+++ basic/operator/code.js
@@ -1,2 +1,9 @@
-// variable in java
-console.log("Hello world");
+// variable
+// A varibale is simply the name of a storage loaction
+//a = 10; a naam ke  varible create key hai usme value 10 hai
+
+
+// Datatype in js
+
+// primitive tyeps
+
