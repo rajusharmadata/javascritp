85eae051bafd5b1f10594fe4d1b43ea0d3f8a4b6
Raju sharma <rajusharmadata123@gmail.com>
2025-01-14 12:19:02 +0530

    add

--- part10/javascritpminiproject/app.js
+++ part10/javascritpminiproject/app.js
@@ -1,14 +1,50 @@
 let btn = document.querySelector("button");
-let li = document.querySelector("li");
 let inp = document.querySelector("input");
 let ul = document.querySelector("ul");
 
 btn.addEventListener("click", function () {
-    let valu = inp.value;
-    let newlist = document.createElement("li");
-    newlist.innerText = valu;
-    ul.appendChild(newlist);
+    // create a list element
+    if (inp.value != "") {
+        let item = document.createElement("li");
+        item.innerText = inp.value;
+
+
+         // delet btn
+    let delbtn = document.createElement("button");
+    delbtn.innerHTML = "delet";
+    delbtn.classList.add("deletbtn");
+
+    // add in item
+    item.appendChild(delbtn);
+    ul.appendChild(item);
+    inp.value = "";
+
+    } else {
+        console.log("don't add task becaue not write waht's dot ")
+   }
 
 
 
+
+
+});
+
+ul.addEventListener("click",function (event) {
+    console.log(event.target.nodeName);
+    if (event.target.nodeName == "BUTTON") {
+        let deltitme = event.target.parentElement;
+        deltitme.remove();
+    } else {
+        console.log("don't delet element");
+    }
 })
+
+
+// delet btn function
+// let btnall = document.querySelectorAll(".deletbtn");
+// for (deletbtn of btnall) {
+//     deletbtn.addEventListener("click", function () {
+//         let par = this.parentElement;
+//         par.remove();
+//     });
+// }
