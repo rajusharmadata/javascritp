6f35d1b0f8180f9d8a27bf3e46c248a16b018876
Raju kumar <rajusharmadata123@gmail.com>
2025-01-25 18:24:56 +0530

    chnage in code

--- part11/index.js
+++ part11/index.js
@@ -1,10 +1,14 @@
 function Hello(){
+    console.log(" inside the Hello functionn");
     console.log("Hello world");
 }
 
 function demo(){
+    console.log(" calling Hello function");
+
      Hello();
 }
 
-// now call the demo
+// now call the demo 
+console.log("calling demo function ");
 demo();
