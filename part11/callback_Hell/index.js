a2372ea689ccced38e896223949dbccbe8d09280
Raju kumar <rajusharmadata123@gmail.com>
2025-02-02 18:10:08 +0530

    modify the file

--- part11/callback_Hell/index.js
+++ part11/callback_Hell/index.js
@@ -3,8 +3,12 @@ let h1 = document.querySelector("h1");
  function changecolur(color,delay ,changecolurfun){
     setTimeout(()=>{
         h1.style.color = color;
-        if(changecolurfun) changecolur();
+        if(changecolurfun) changecolurfun();
     },delay);
  }
 
- changecolur(red,1000);
+ changecolur("red",1000, ()=>{
+    changecolur("green",1000,()=>{
+        changecolur("yellow",1000);
+    })
+ });
